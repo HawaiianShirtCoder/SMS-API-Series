cc1cbf739c461768155c0aef33b923e33446e0b2
HainesJason <97175397+HainesJason@users.noreply.github.com>
2023-01-06 17:05:14 +0000

    Added database project

---
+++ SMS.Database/dbo/Tables/Availability.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Availability] (
+    [Id]        INT IDENTITY (1, 1) NOT NULL,
+    [FixtureId] INT NOT NULL,
+    [PlayerId]  INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Availability_Fixture] FOREIGN KEY ([FixtureId]) REFERENCES [dbo].[Fixture] ([Id]),
+    CONSTRAINT [FK_Availability_Player] FOREIGN KEY ([PlayerId]) REFERENCES [dbo].[Player] ([Id])
+);
+
---
+++ SMS.Database/dbo/Tables/Player.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Player] (
+    [Id]             INT            IDENTITY (1, 1) NOT NULL,
+    [Firstname]      NVARCHAR (MAX) NOT NULL,
+    [Lastname]       NVARCHAR (MAX) NOT NULL,
+    [Email]          NVARCHAR (MAX) NOT NULL,
+    [PhoneNumber]    NVARCHAR (50)  NULL,
+    [IsActivePlayer] BIT            DEFAULT ((1)) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
