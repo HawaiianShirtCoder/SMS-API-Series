f29f76e1699f5b1e27785849cc297a65e118e3a1
HainesJason <97175397+HainesJason@users.noreply.github.com>
2023-01-24 19:05:30 +0000

    Added a SQL Lite database to the shared project for demo purposes

---
+++ SMS.Shared/DAL/SqlLiteDapperDataAccess.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+
+namespace SMS.Shared.DAL;
+
+
+public class SqlLiteDapperDataAccess : IDataAccess
+{
+    public Task ExecuteACommand<T>(string sqlStatement, T parameters, string connectionString, CommandType commandType = CommandType.Text)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<T>> RunAQuery<T, U>(string sqlStatement, U parameters, string connectionString, CommandType commandType = CommandType.Text)
+    {
+        throw new NotImplementedException();
+    }
+}
